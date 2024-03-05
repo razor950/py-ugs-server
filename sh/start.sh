4d8268ae30024a8e4ff96ca8018e1e755750e425
razor950 <razor950@gmail.com>
2024-03-05 10:20:35 -0500

    start script uses environment variables

--- sh/start.sh
+++ sh/start.sh
@@ -4,7 +4,7 @@
 cd /data
 
 # make sure mysql ready
-./sh/wait-for-it.sh ugs-mysql:3306 -t 30 -- echo "mysql server is ready, wait for another 3 seconds to make sure the init script is done..." && sleep 3s
+./sh/wait-for-it.sh ${DB_UGS_HOST}:${DB_UGS_PORT} -t 30 -- echo "mysql server is ready, wait for another 3 seconds to make sure the init script is done..." && sleep 3s
 
 # run!
 python ./api/main.py
